584baca097a88224009c0cb1803aff9f616cc7d2
Ram Babu <ram.kottapally@gmail.com>
2019-10-18 15:07:20 +0530

    Initial version
    Given a location in India, this script scrapes the web and show the weather forecast for 3 days. The Weather forecast includes -
    - Max Temperature
    - Min Temperature
    - Weather

---
+++ Weather_forecast_application.py
@@ -0,0 +1,57 @@
+#Weather Scrapper - Python
+'''
+Sources: https://stackoverflow.com/questions/6871016/adding-5-days-to-a-date-in-python
+1. Take in the country name and city name.
+2. Store the dates as keys in a dictionary. Values should contain a dict that
+contains the (Min, max, feels like, weather description - cloudy etc.,)
+{date: {min:15, max: 19, feels_like: 17, weather_desc: Cloudy}}
+3. Print the data.
+'''
+#Necessary Modules
+import requests
+import bs4 as bs
+import datetime
+import sys
+
+#Dictionary to store the Weather Details. Date as Keys and details as the value stored as a list.
+weather_details = {}
+
+#Current Date
+curr_date = datetime.datetime.now().strftime("%d-%b-%Y")
+date_1 = datetime.datetime.strptime(curr_date, "%d-%b-%Y")
+
+#Using a browser Agent
+headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"}
+area = input('Enter a Indian Location: ').strip().lower()
+
+#Getting the webpage of the URL
+url = 'https://www.timeanddate.com/weather/india/' + area + '/ext'
+response = requests.get(url, headers = headers)
+html = response.text
+
+#Extracting the necessary information
+soup = bs.BeautifulSoup(html, 'lxml')
+temp = soup.find('tbody')
+
+#Looping and extracting the Information about weather.
+for i in range(3):
+    end_date = date_1 + datetime.timedelta(days=i)
+    try:
+        temperature = temp.select_one("tr:nth-of-type(" + str(i+1) + ")").select_one("td:nth-of-type(2)").text
+        weather_details[end_date.strftime("%d-%b-%Y")] = [temperature]
+        weather_details[end_date.strftime("%d-%b-%Y")].append(temp.select_one("tr:nth-of-type(" + str(i+1) + ")").select_one("td:nth-of-type(3)").text)
+        weather_details[end_date.strftime("%d-%b-%Y")].append(temp.select_one("tr:nth-of-type(" + str(i+1) + ")").select_one("td:nth-of-type(4)").text)
+    except AttributeError:
+        print('Invalid Location. Please Enter a popular location.')
+        sys.exit(8)
+
+
+print('Weather Forecast for {}'.format(area.title()))
+print('-'*35)
+for i in weather_details:
+    print(i, *weather_details[i])
+
+
+
+
+
